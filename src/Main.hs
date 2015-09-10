4ea90a51629c90d62b296ca1618ef41d1e7b50da
Atila Neves <aalvesne@cisco.com>
2015-09-10 19:36:05 +0200

    Up to exercise 4, except for binary literals

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,7 @@ module Main where
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
 import Control.Monad
+import Numeric
 
 data LispVal = Atom String
              | List [LispVal]
@@ -10,10 +11,11 @@ data LispVal = Atom String
              | Number Integer
              | String String
              | Bool Bool
+               deriving(Show)
 
 
 symbol :: Parser Char
-symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+symbol = oneOf "!$%&|*+-/:<=>?@^_~"
 
 
 spaces :: Parser ()
@@ -23,9 +25,21 @@ spaces = skipMany1 space
 parseString :: Parser LispVal
 parseString = do
   char '"'
-  x <- many (noneOf "\"")
+  x <- many $ escapedChars <|> noneOf "\"\\"
   char '"'
   return $ String x
+
+
+escapedChars :: Parser Char
+escapedChars = do
+  char '\\'
+  x <- oneOf "\\\"nrt"
+  return $ case x of
+             '\\' -> x
+             '"' -> x
+             'n' -> '\n'
+             'r' -> '\r'
+             't' -> '\t'
 
 
 parseAtom :: Parser LispVal
@@ -39,19 +53,45 @@ parseAtom = do
              _ -> Atom atom
 
 
+parseBool :: Parser LispVal
+parseBool = do
+  char '#'
+  (char 't' >> return (Bool True)) <|> (char 'f' >> return (Bool False))
+
+
 parseNumber :: Parser LispVal
-parseNumber = liftM (Number . read) $ many1 digit
+parseNumber = parseDecimal <|> parseDecimalHash <|> parseOctal <|> parseHex
+
+
+parseDecimal :: Parser LispVal
+parseDecimal = liftM (Number . read) $ many1 digit
+
+parseDecimalHash :: Parser LispVal
+parseDecimalHash = try $ string "#d" >> parseDecimal
+
+parseOctal :: Parser LispVal
+parseOctal = do try $ string "#o"
+                x <- many1 octDigit
+                return $ (Number . firstOfFirst . readOct) x
+
+parseHex :: Parser LispVal
+parseHex = do try $ string "#x"
+              x <- many1 hexDigit
+              return $ (Number . firstOfFirst . readHex) x
+
+
+firstOfFirst x = fst $ x !! 0
 
 
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> parseNumber
+parseExpr = parseAtom <|> parseString <|> parseNumber <|> parseBool
 
 
 readExpr :: String -> String
 readExpr input =
     case parse parseExpr "lisp" input of
       Left err -> "No match: " ++ show err
-      Right val -> "Found value"
+      Right val -> "Found value of " ++ show val
 
 
 main :: IO ()
