a99bd02aba264c3894ba9f16c9c75b27a86c233e
Atila Neves <aalvesne@cisco.com>
2015-09-10 18:31:54 +0200

    Symbol parser

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,18 @@
 module Main where
 
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+symbol :: Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+readExpr :: String -> String
+readExpr input =
+    case parse symbol "lisp" input of
+      Left err -> "No match: " ++ show err
+      Right val -> "Found value"
+
 main :: IO ()
-main = return ()
+main = do
+  (expr:_) <- getArgs
+  putStrLn $ readExpr expr
